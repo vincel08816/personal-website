ab293495d271dbfc0ec7887ab0349465b865c750
Vincent Lee <vince.l08816@gmail.com>
2022-07-09 16:48:19 -0400

    fixed a couple divs

--- src/Pages/Home/Display.jsx
+++ src/Pages/Home/Display.jsx
@@ -10,24 +10,26 @@ export const Display = () => {
     <>
       <div ref={aboutRef} style={{ marginTop: "-30px", height: "60px" }} />
       <Container>
-        <Left />
-        <Right />
+        <InnerContainer>
+          <Left />
+          <Right />
+        </InnerContainer>
       </Container>
     </>
   );
 };
 
-const Container = styled.div`
+const InnerContainer = styled.div`
   display: flex;
   border-radius: 15px;
   margin: 25px 0 75px 0;
   background-color: white;
-  width: 95%;
-  max-width: 1200px;
   min-height: 600px;
-  flex: 1;
   box-shadow: 1px 6px 12px hsl(0, 0%, 75%);
+`;
 
+const Container = styled.div`
+  max-width: 1200px;
   /* makes text unhighlightable */
   -webkit-touch-callout: none;
   -webkit-user-select: none;
