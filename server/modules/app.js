9e9ada11718f9321b532a6e752210eccb5449cad
unknown <vince.l08816@gmail.com>
2022-07-23 18:13:05 -0400

    a;sdlfkj

--- server/modules/app.js
+++ server/modules/app.js
@@ -7,36 +7,33 @@ const cp = require("cookie-parser");
 const app = express();
 const path = require("path");
 
-app.use(passport.initialize())
-require('./passport-config')(passport)
+app.use(passport.initialize());
+require('./passport-config')(passport);
 
-app.use(cp())
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(cp());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-function ensureHttps(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    return next()
-  }
-  res.redirect('https://' + req.hostname + req.url)
-}
 
 if (process.env.PRODUCTION === '1') {
-  app.all('*', ensureHttps)
+  app.all('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] === 'https') return next();
+    res.redirect('https://' + req.hostname + req.url);
+  })
 }
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
-  )
-  next()
+  );
+  next();
 })
 
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 app.use("/message", require("../routes/message"));
 app.use("/auth", require("../routes/auth"));
